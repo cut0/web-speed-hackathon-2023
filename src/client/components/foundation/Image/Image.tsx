462919fa09e30aa7e79b745aa6cf7c3a9ed8a2c1
cut0 <zeron9421@gmail.com>
2023-03-04 14:53:37 +0900

    画像に loading 追加

--- src/client/components/foundation/Image/Image.tsx
+++ src/client/components/foundation/Image/Image.tsx
@@ -5,15 +5,16 @@ import * as styles from './Image.styles';
 
 type Props = Omit<ComponentProps<'img'>, 'className'> & {
   fill?: boolean;
+  loading?: 'lazy' | 'eager';
 };
 
-export const Image: FC<Props> = ({ fill, ...rest }) => {
+export const Image: FC<Props> = ({ fill, loading = 'eager', ...rest }) => {
   return (
     <img
       className={classNames(styles.container(), {
         [styles.container__fill()]: fill === true,
       })}
-      loading="eager"
+      loading={loading}
       {...rest}
     />
   );
